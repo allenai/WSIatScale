2f7285367c8a14ea89a11583cea40bf336b7849c
mataney <mataneyal1@gmail.com>
2021-11-28 11:48:01 +0200

    add communities

--- WSIatScale/apriori.py
+++
@@ -1,6 +0,0 @@
-from efficient_apriori import itemsets_from_transactions
-
-def run_apriori(reps_to_instances, min_support):
-    keys = [k for k, v in reps_to_instances.data.items() for _ in range(len(v))]
-    itemsets, _ = itemsets_from_transactions(keys, min_support=min_support, output_transaction_ids=True)
-    return itemsets
